08fbd20b87832a7881934fac866ad9115cdde334
Onischuck Mikhail <onischuck.misha@yandex.ru>
2018-10-01 02:52:27 +0300

    Solution for Task1_1 and new tests for it

--- Task1_1.hs
+++ Task1_1.hs
@@ -2,26 +2,55 @@ module Task1_1 where
 
 import Todo(todo)
 
+data Operation = Plus | Minus | Times deriving(Show,Eq)
+
 data Term = IntConstant{ intValue :: Int }           -- числовая константа
             | Variable{ varName :: String }          -- переменная
-            | BinaryTerm{ lhv :: Term, rhv :: Term } -- бинарная операция
+            | BinaryTerm{ lhv :: Term, rhv :: Term, ac :: Operation } -- бинарная операция
             deriving(Show,Eq)
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
-(|+|) l r = todo
+(|+|) l r = BinaryTerm l r Plus
 (|-|) :: Term -> Term -> Term
-(|-|) l r = todo
+(|-|) l r = BinaryTerm l r Minus
 (|*|) :: Term -> Term -> Term
-(|*|) l r = todo
+(|*|) l r = BinaryTerm l r Times
+
+infixl 1 |+|
+infixl 1 |-|
+infixl 2 |*|
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = todo
+replaceVar varName replacement expression = case expression of
+  Variable v | v == varName -> replacement
+  BinaryTerm l r a -> BinaryTerm (replaceVar varName replacement l) (replaceVar varName replacement r) a
+  _ -> expression
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate expression = todo
+evaluate expression = case expression of
+  BinaryTerm left right op -> do
+    let l = evaluate left
+    let r = evaluate right
+    case (l, r) of
+      -- для упрощения выражения, если не можем посчитать
+      (            _, IntConstant 0) | op == Plus || op == Minus -> l
+      (            _, IntConstant 0) | op == Times -> IntConstant 0
+      (            _, IntConstant 1) | op == Times -> l
+      (IntConstant 0,             _) | op == Plus  -> r
+      (IntConstant 0,             _) | op == Times -> IntConstant 0
+      (IntConstant 1,             _) | op == Times -> r
+      -- непосредственно вычисление
+      (IntConstant a, IntConstant b) -> 
+        case op of
+          Plus  -> IntConstant $ a + b
+          Minus -> IntConstant $ a - b
+          Times -> IntConstant $ a * b
+      -- не можем упростить или вычислить
+      _ -> BinaryTerm l r op
+  _ -> expression
--- main.hs
+++ main.hs
@@ -3,27 +3,48 @@ import Task1_1
 import Task1_2
 
 main = do
-  putStr "Sine tests:\n"
+  putStrLn "Task1_1:"
+  -- (A - 1) * 2 - A * (3 + 4 + 5 - 6 * 7) = (A - 1) * 2 - A * (-30)
+  let a = (Variable "A" |-| IntConstant 1) |*| IntConstant 2 |-| Variable "A" |*| (IntConstant 3 |+| IntConstant 4 |+| IntConstant 5 |-| IntConstant 6 |*| IntConstant 7)
+  putStrLn "a:";          print $ a;          putStrLn "---"
+  putStrLn "evaluate a:"; print $ evaluate a; putStrLn "---"
+
+  -- (0 - 1) * 2 - 0 * (3 + 4 + 5 - 6 * 7) = -2
+  let b = replaceVar "A" (IntConstant 0) a
+  putStrLn "replaceVar:"; print $ b;          putStrLn "---"
+  putStrLn "evaluate b:"; print $ evaluate b; putStrLn "---"
+  
+  --- 1 * (1 * Z + 0) + 0 = Z
+  let c = IntConstant 1 |*| (IntConstant 1 |*| Variable "Z" |+| IntConstant 0) |+| IntConstant 0
+  putStrLn "c:";          print $ c;          putStrLn "---"
+  putStrLn "evaluate c:"; print $ evaluate c; putStrLn "---"
+
+  -- тесы для задания №2
+
+  putStrLn "Sine tests:"
   putStr "sin( 0   ) = "; print $ Task1_2.sin ( 0   )
   putStr "sin( pi/2) = "; print $ Task1_2.sin ( pi/2)
   putStr "sin(-pi/2) = "; print $ Task1_2.sin (-pi/2)
   putStr "sin( pi  ) = "; print $ Task1_2.sin ( pi  )
+  putStrLn ""
 
-  putStr "\nCosine tests:\n"
+  putStrLn "Cosine tests:"
   putStr "cos( 0   ) = "; print $ Task1_2.cos ( 0   )
   putStr "cos( pi/2) = "; print $ Task1_2.cos ( pi/2)
   putStr "cos(-pi/2) = "; print $ Task1_2.cos (-pi/2)
   putStr "cos( pi  ) = "; print $ Task1_2.cos ( pi  )
-  
-  putStr "\nGCD tests:\n"
+  putStrLn ""
+
+  putStrLn "GCD tests:"
   putStr "gcd( 27, 45) = "; print $ Task1_2.gcd   27    45
   putStr "gcd( 45, 27) = "; print $ Task1_2.gcd   45    27
   putStr "gcd( -5, 10) = "; print $ Task1_2.gcd  (-5)   10
   putStr "gcd( 10, -5) = "; print $ Task1_2.gcd   10   (-5)
   putStr "gcd(-10, -5) = "; print $ Task1_2.gcd (-10)  (-5)
   putStr "gcd( -5,-10) = "; print $ Task1_2.gcd  (-5) (-10)
-  
-  putStr "\nPOW tests:\n"
+  putStrLn ""
+
+  putStrLn "POW tests:"
   putStr "pow(2, 0) = "; print $ Task1_2.pow 2 0
   putStr "pow(2, 1) = "; print $ Task1_2.pow 2 1
   putStr "pow(2, 2) = "; print $ Task1_2.pow 2 2
@@ -32,11 +53,11 @@ main = do
   putStr "pow(2, 5) = "; print $ Task1_2.pow 2 5
   putStr "pow(2, 5) = "; print $ Task1_2.pow 2 10
   putStr "pow(2,-8) = "; print $ Task1_2.pow 2 (-8)
+  putStrLn ""
 
-  putStr "\nisPrime tests:\n"
+  putStrLn "isPrime tests:"
   putStr "isPrime(17) = "; print $ Task1_2.isPrime ( 19)
   putStr "isPrime(21) = "; print $ Task1_2.isPrime ( 21)
   putStr "isPrime( 1) = "; print $ Task1_2.isPrime (  1)
   putStr "isPrime( 0) = "; print $ Task1_2.isPrime (  0)
   putStr "isPrime(-1) = "; print $ Task1_2.isPrime ( -1)
-

a5cc15c4d86d839a5d8ffaa0ca467a1982e0a623
Onischuck Mikhail <onischuck.misha@yandex.ru>
2018-10-27 19:11:54 +0300

    Managing repository [stage 2]

--- Task2_2.hs
+++ Task2_2.hs
@@ -6,13 +6,20 @@ import Prelude hiding (foldl, foldr, unfoldr, map, concatMap,
     filter, maxBy, minBy, reverse, sum, product, elem)
 
 foldl :: (b -> a -> b) -> b -> [a] -> b
-foldl = todo
+foldl func acc [] = acc
+foldl func acc (h:t) = foldl func (func acc h) t
 
 foldr :: (a -> b -> b) -> b -> [a] -> b
-foldr = todo
+foldr func acc [] = acc 
+foldr func acc (h:t) = func h (foldr func acc t)
 
 unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
-unfoldr = todo
+unfoldr func a list = 
+  case b of
+    Nothing    -> list
+    Just(a, b) -> a:(unfoldr func b (a:lst))
+  where
+    b = func a
 
 -- Сумма всех элементов списка (пример)
 sum :: [Integer] -> Integer
@@ -24,15 +31,18 @@ reverse lst = foldl f [] lst where f t h = h:t
 
 -- Отображение элементов списка
 map :: (a -> b) -> [a] -> [b]
-map = todo
+map func list = foldr ( \x a -> ((func x):a) ) [] list
 
 -- Произведение всех элементов списка
 product :: [Integer] -> Integer
-product = todo
+product = foldl (*) 1 lst
 
 -- Выделение из списка Maybe всех существующих значений
 catMaybes :: [Maybe a] -> [a]
-catMaybes = todo
+catMaybes = foldl f [] lst
+  where
+    f t (Just a) = a:t
+    f t Nothing  = t
 
 -- Диагональ матрицы
 diagonal :: [[a]] -> [a]
@@ -52,7 +62,7 @@ rangeTo from to step = todo
 
 -- Конкатенация двух списков
 append :: [a] -> [a] -> [a]
-append = todo
+append a b = foldr(\x a -> x: a) b a
 
 -- Разбиение списка lst на куски размером n
 -- (последний кусок может быть меньше)

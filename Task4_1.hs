50b6f28dccba47d8e5b0766e41189182337c3ead
Onischuck Mikhail <onischuck.misha@yandex.ru>
2018-12-01 17:54:35 +0300

    Solution for Task4 (just convoluted hell)

--- Task4_1.hs
+++ Task4_1.hs
@@ -5,3 +5,27 @@ module Task4_1 where
 data FunMonad a = FunMonad { fun :: String -> a }
 
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FunMonad`
+
+instance Functor FunMonad where
+  -- fmap :: (a -> b) -> FunMonad a -> FunMonad b
+  fmap f (FunMonad x) = FunMonad (\s -> f $ x s)
+
+instance Applicative FunMonad where
+  -- pure :: a -> FunMonad a
+  pure x = return x
+
+  -- (<*>) :: FunMonad (a -> b) -> FunMonad a -> FunMonad b
+  -- где a = fun::String -> a, т.е. "по-человечески":
+  -- [in ] f   = F (String, a): b, <- функция, принимающая строку и результат старой функции
+  -- [in ] x   = fun (String): a,  <- старая ф-ция, принимавшая строку и возвращавшая A
+  -- [out] res = fun (String): b   <- новая ф-ция, принимающая строку и возвращающая B
+  (<*>) (FunMonad f) (FunMonad x) = FunMonad (\s -> f s $ x s)
+
+instance Monad FunMonad where
+  -- (>>=) :: FunMonad a -> (a -> FunMonad b) -> FunMonad b
+  -- FM (String -> a) -> (a -> FM (String -> b)) -> FM (String -> b),
+  -- при том что на самом деле: fun:: FunMonad a -> String -> a
+  (>>=) (FunMonad x) f = FunMonad (\s -> fun (f $ x s) s)
+
+  -- return :: a -> FunMonad a
+  return x = FunMonad (\ _ -> x)
--- Task4_2.hs
+++ Task4_2.hs
@@ -5,3 +5,26 @@ data FourOf a = FourOf a a a a deriving(Show,Eq)
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FourOf`
 -- таким образом, что 
 -- do { x <- FourOf 1 2 3 4; y <- FourOf 4 6 7 8; return $ x + y } === FourOf 5 8 10 12
+
+instance Functor FourOf where
+  -- fmap :: (a -> b) -> Functor a -> Functor b
+  fmap f (FourOf x1 x2 x3 x4) = FourOf (f x1) (f x2) (f x3) (f x4)
+
+instance Applicative FourOf where
+  pure = return
+
+  -- (<*>) :: Functor (a -> b) -> Functor a -> Functor b
+  (<*>) (FourOf f1 f2 f3 f4) (FourOf x1 x2 x3 x4) = FourOf (f1 x1) (f2 x2) (f3 x3) (f4 x4)
+
+
+instance Monad FourOf where
+  -- (>>=) :: Monad a -> (a -> Monad b) -> Monad b
+  (>>=) (FourOf x1 x2 x3 x4) f = FourOf y1 y2 y3 y4
+    where
+      FourOf y1 _ _ _ = f x1
+      FourOf _ y2 _ _ = f x2
+      FourOf _ _ y3 _ = f x3
+      FourOf _ _ _ y4 = f x4
+
+  -- return :: a -> m a
+  return x = FourOf x x x x

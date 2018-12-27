5b60d77986439dcc034b6b1241131baf793f8bf8
Onischuck Mikhail <onischuck.misha@yandex.ru>
2018-12-27 16:19:33 +0300

    Added text of the Task_6

--- Task6.hs
+++ Task6.hs
@@ -1,12 +1,21 @@
 module Task6 where
 
+{-
+  необходимо реализовать тип данных, представл€ющий собой бинарное дерево поиска
+  (по мотивам соответствующей структуры данных из лекций), но такое, в котором в каждой €чейке
+  содержитс€ ссылка на родительскую €чейку (подобно тому, как в типе DList содержитс€ ссылка
+  на предыдущий элемент).
+-}
+
 import Todo(todo)
 
 data LinkedTree a = ChangeMe
 
 find :: LinkedTree a -> a -> Bool
 find = todo
+
 insert :: LinkedTree a -> a -> LinkedTree a
 insert = todo
+
 remove :: LinkedTree a -> a -> LinkedTree a
 remove = todo

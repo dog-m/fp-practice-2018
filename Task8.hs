2736d8997d90e9a692d86bc99885893b1093f08d
Onischuck Mikhail <onischuck.misha@yandex.ru>
2018-12-29 19:18:18 +0300

    Now Task8 kinda works

--- Task8.hs
+++ Task8.hs
@@ -2,7 +2,7 @@
 представлен парсер, разработанный нами на лекции.
 Модифицируйте его таким образом, чтобы он поддерживал:
   + Числа с плавающей точкой
-  * Операцию унарного минуса
+  + Операцию унарного минуса
   + Операцию возведения в степень
 -}
 
@@ -15,32 +15,33 @@ type Parser a = Parsec String () a
 digit :: Parser Char
 digit = oneOf $ ['0'..'9']
 
-(<++>) a b = (++) <$> a <*> b
-(<:>) a b = (:) <$> a <*> b
-
 fraction :: Parser String
 fraction = do
   char '.'
   f <- many1 digit
   return $ "." ++ f
 
+haveMinusSign :: Parser Bool
+haveMinusSign = do
+  char '-'
+  return True
+
 number :: Parser Double
 number = do
-  a <- many1 digit
-  frac <- option "" fraction
+  a     <- many1 digit
+  frac  <- option "" fraction
   return $ read $ a ++ frac
 
 byNumber ::  Char 
           -> (Double -> Double -> Double) 
           -> Parser Double
           -> Parser (Double -> Double)
-byNumber symbol func base =
-  do
-    char symbol
-    spaces
-    n <- base
-    spaces
-    return $ (`func` n)
+byNumber symbol func base = do
+  char symbol
+  spaces
+  n <- base
+  spaces
+  return $ (`func` n)
 
 multNumber :: Parser (Double -> Double)
 multNumber = byNumber '*' (*) expr
@@ -52,12 +53,11 @@ powNumber :: Parser (Double -> Double)
 powNumber = byNumber '^' (**) expr
 
 multiplication :: Parser Double
-multiplication =
-  do
-    x <- expr
-    spaces
-    ys <- many (multNumber <|> divNumber <|> powNumber)
-    return $ foldl (\ x f -> f x) x ys
+multiplication = do
+  x <- expr
+  spaces
+  ys <- many (multNumber <|> divNumber <|> powNumber)
+  return $ foldl (\ x f -> f x) x ys
 
 plusNumber :: Parser (Double -> Double)
 plusNumber = byNumber '+' (+) multiplication
@@ -66,37 +66,36 @@ minusNumber :: Parser (Double -> Double)
 minusNumber = byNumber '-' (-) multiplication
 
 addition :: Parser Double
-addition =
-  do
-    x <- multiplication
-    spaces
-    ys <- many (plusNumber <|> minusNumber)
-    return $ foldl (\ x f -> f x) x ys
+addition = do
+  x <- multiplication
+  spaces
+  ys <- many (plusNumber <|> minusNumber)
+  return $ foldl (\ x f -> f x) x ys
 
 expr :: Parser Double
-expr =
-  number <|> expression
+expr = do
+  minus <- option False haveMinusSign
+  x <- number <|> expression
+  return $ if minus then (-x) else x
 
 expression :: Parser Double
-expression =
-  do
-    char '('
-    spaces
-    res <- addition
-    char ')'
-    spaces
-    return $ res
+expression = do
+  spaces
+  char '('
+  spaces
+  res <- addition
+  char ')'
+  spaces
+  return res
 
 root :: Parser Double
-root =
-  do
-    spaces
-    p <- addition
-    eof
-    return $ p
+root = do
+  spaces
+  p <- addition
+  eof
+  return $ p
 
-main =
-  do
-    s <- getLine
-    putStrLn $ show $ parse root "<input>" s
-    -- main
+main = do
+  s <- getLine
+  putStrLn $ show $ parse root "<input>" s
+  -- main
